ff055a378753c6f97999f7eab4041dc8f9e263a9
= <=>
2024-08-03 16:05:08 -0300

    fix ejercicio 2 y eliminacion de codigo extra

--- programa.pl
+++ programa.pl
@@ -3,7 +3,7 @@
 % 1. Modelo de jugadores, civilizaciones y tecnologías 
 % Datos: 
 % - Personas: Ana, Beto, Carola, Dimitri, Elsa
-% - Civilizaciones: romanos, incas,
+% - Civilizaciones: romanos, incas
 % - Tecnologias: herrería, forja, emplumado, láminas, fundición.
 
 % Modelando jugadores: 
@@ -12,21 +12,25 @@ jugador(beto).
 jugador(carola).
 jugador(dimitri).
 jugador(elsa).
+
 % Modelando civilizaciones:
 civilizacion(romanos).
 civilizacion(incas).
+
 % Modelando tecnologías:
 tecnologia(herreria).
 tecnologia(forja).
 tecnologia(emplumado).
 tecnologia(laminas).
 tecnologia(fundicion).
+
 % Asociando jugadores a civilizacion:
 juegaPartida(ana, romanos).
 juegaPartida(beto, incas).
 juegaPartida(carola, romanos).
 juegaPartida(dimitri, romanos).
 % juegaPartida(elsa, -). elsa queda excluida de la partida, pues no juega
+
 % Asociando jugadores con tecnologia desarrollada:
 desarrollaTecnologia(ana, herreria).
 desarrollaTecnologia(ana, forja).
@@ -41,14 +45,20 @@ desarrollaTecnologia(carola, herreria).
 
 desarrollaTecnologia(dimitri, herreria).
 desarrollaTecnologia(dimitri, fundicion).
+
 % Asociando jugador, civilizacion y tecnologias:
-juegaConCivilizacion(Jugador, Civilizacion):- jugador(Jugador), juegaPartida(Jugador, Civilizacion). 
+juegaConCivilizacion(Jugador, Civilizacion):- juegaPartida(Jugador, Civilizacion). 
+habilidadTecnologica(Jugador, Tecnologia):- desarrollaTecnologia(Jugador, Tecnologia).
 
-habilidadTecnologica(Jugador, Tecnologia):- jugador(Jugador), desarrollaTecnologia(Jugador, Tecnologia).
-
-% 2. esExperto en metales: cuando? → desarollaTecnologia(herreria, forja, fundicion) o civilizacion(romanos) 
-esExpertoEnMetales(Jugador):- jugador(Jugador), desarrollaTecnologia(Jugador, [herreria, forja, fundicion]).
-esExpertoEnMetales(Jugador):- jugador(Jugador), juegaPartida(Jugador, romanos).
+% 2. Es experto en metales cuando desarollaTecnologia(herreria, forja, fundicion) o desarollaTecnologia(herreria, forja) y civilizacion(romanos) 
+esExpertoEnMetales(Jugador):-
+    desarrollaTecnologia(Jugador, herreria),
+    desarrollaTecnologia(Jugador, forja),
+    desarrollaTecnologia(Jugador, fundicion).
+esExpertoEnMetales(Jugador):-
+    desarrollaTecnologia(Jugador, herreria),
+    desarrollaTecnologia(Jugador, forja),
+    juegaPartida(Jugador, romanos).
 
 % 3. civilizacionPopular: cuando? → +1 jugador la eligen 
 civilizacionPopular(Civilizacion):- (length(juegaPartida(Civilizacion, _), cantidadJugadores), cantidadJugadores > 1).
