a5ba6d277c0456aeafef3f3983ffcefdaadbdc58
DamAng91 <dangelucci@frba.utn.edu.ar>
2024-08-04 14:29:58 -0300

    Ejercicio 5.

--- programa.pl
+++ programa.pl
@@ -50,7 +50,7 @@ desarrollaTecnologia(dimitri, fundicion).
 juegaConCivilizacion(Jugador, Civilizacion):- juegaPartida(Jugador, Civilizacion). 
 habilidadTecnologica(Jugador, Tecnologia):- desarrollaTecnologia(Jugador, Tecnologia).
 
-% 2. Es experto en metales cuando desarollaTecnologia(herreria, forja, fundicion) o desarollaTecnologia(herreria, forja) y civilizacion(romanos) 
+% 2. Es experto en metales cuando desarrollaTecnologia(herreria, forja, fundicion) o desarrollaTecnologia(herreria, forja) y civilizacion(romanos) 
 esExpertoEnMetales(Jugador):-
     desarrollaTecnologia(Jugador, herreria),
     desarrollaTecnologia(Jugador, forja),
@@ -64,7 +64,12 @@ esExpertoEnMetales(Jugador):-
 cantidadJugadores(Civilizacion, Cantidad):- findall(Jugador, juegaPartida(Jugadores, Civilizacion), Jugadores), length(Jugadores, Cantidad).
 civilizacionPopular(Civilizacion):- cantidadJugadores(Civilizacion, Cantidad), Cantidad > 1.
 
-% 5. civilizacionLider: cuando? → la civilizacion alcanzo TODAS (6) las tecnologias; esto es, para una misma civilizacion, sumo las tecnologias de cada jugador y si completan todas las tecnologias => es lider. 
-cantidadTecnologias(Civilizacion, Cantidad):- findall(Jugador, habilidadTecnologica(Jugador, Tecnologia),   )
+% 5. civilizacionLider: cuando? → la civilizacion alcanzo TODAS (5) las tecnologias; esto es, para una misma civilizacion, sumo las tecnologias de cada jugador y si completan todas las tecnologias => es lider. 
+% listaTotalTecnologias:
+listaTotalTecnologias(ListaTotalTecno):- findall(Tecnologia, desarrollaTecnologia(_, Tecnologia), Tecnologias), list_to_set(Tecnologias, ListaTotalTecno).
+% lista de Tecnologias por civilizacion: 
+listaTecnologias(Civilizacion, ListaTecno):- findall(Tecnologia, (juegaPartida(Jugador, Civilizacion), desarrollaTecnologia(Jugador, Tecnologia)), Tecnologias), list_to_set(Tecnologias, ListaTecno).
+% Modelando si una civilizacion es lider:
+civilizacionLider(Civilizacion):- listaTecnologias(Civilizacion, TecnoCivilizacion), listaTotalTecnologias(TotalTecnos), subset(TecnoCivilizacion, TotalTecnos).
 
-% civilizacionLider(Tecnologia, Cantidad):- 
+
